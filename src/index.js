7ace6c91fd6bb42994ce3236830e4b4e690808a9
NNickutckina <n.nickutckina@yandex.ru>
2015-12-22 00:35:53 +0300

    Revert "weatherlab3"
    This reverts commit 6d2247ade63b41dfbb082edc48f220a4f486a259.

--- src/index.js
+++ src/index.js
@@ -1,37 +1,15 @@
-var showTable = function() {
-	$(".result-table").show();
-};
-
-var hideTable = function() {
-	$(".result-table").hide();
-};
-
-var renderTable = function(weatherList) {
-	$(".result").html(
-		$( "#weatherTemplate" ).render( weatherList )
-	);
-};
-
 $(function() {
   var resultHandler = function(data) {
-	if (isServerAnswerIncorrect(data)) {
-		alert("Server is crazy!");
-		return;
-	}
-	showTable();
-	renderTable(data.list);
+    $(".result-table").show();
+	$(".result").html(
+		$( "#weatherTemplate" ).render( data.list )
+	);
   };
   
   $(".search")
     .on("click", function() {
-		hideTable();
-        var cityName = $(".city").val();
-		if (isCityNameIncorrect(cityName)) {
-			alert("Try again, type some more letters!");
-			return;
-		}
-		
-        var url = 'http://api.openweathermap.org/data/2.5/forecast?q='+ cityName + '&units=metric&lang=ru&appid=2de143494c0b295cca9337e1e96b00e0';
+        var cityName = $(".city").val(),
+            url = 'http://api.openweathermap.org/data/2.5/forecast?q='+ cityName + '&units=metric&lang=ru&appid=2de143494c0b295cca9337e1e96b00e0';
 		$.ajax({
             url: url,
             type: 'GET',
--- src/utils.js
+++
@@ -1,7 +0,0 @@
-var isCityNameIncorrect = function(name) {
-	return (name === "" || name.length < 2);
-};
-
-var isServerAnswerIncorrect = function(data) {
-	return (data === "" || data.cod === "404");
-};
