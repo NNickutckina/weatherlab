a408b5cb47fa6f4b6f3374723c4857f464452aae
NNickutckina <n.nickutckina@yandex.ru>
2015-12-22 03:49:48 +0300

    angular is added

--- src/index.js
+++ src/index.js
@@ -1,41 +1,33 @@
-var showTable = function() {
-	$(".result-table").show();
-};
-
-var hideTable = function() {
-	$(".result-table").hide();
-};
-
-var renderTable = function(weatherList) {
-	$(".result").html(
-		$( "#weatherTemplate" ).render( weatherList )
-	);
-};
-
-$(function() {
-  var resultHandler = function(data) {
-	if (isServerAnswerIncorrect(data)) {
-		alert("Server is crazy!");
-		return;
-	}
-	showTable();
-	renderTable(data.list);
-  };
-  
-  $(".search")
-    .on("click", function() {
-		hideTable();
-        var cityName = $(".city").val();
-		if (isCityNameIncorrect(cityName)) {
-			alert("Try again, type some more letters!");
-			return;
+(function() {
+    var app = angular.module('wheatherApp', []);
+	
+	app.controller('MainCtrl', function($scope, $http, $timeout) {
+		$scope.showTable = false;
+		$scope.units = 'metric';
+		$scope.days = 2;
+		$scope.lang = 'ru';
+		$scope.getTime = function(time) {
+			var date = new Date(time * 1000);
+			return date.toLocaleString();
 		}
-		
-        var url = 'http://api.openweathermap.org/data/2.5/forecast?q='+ cityName + '&units=metric&lang=ru&appid=2de143494c0b295cca9337e1e96b00e0';
-		$.ajax({
-            url: url,
-            type: 'GET',
-            dataType: 'json'
-        }).done(resultHandler);                
+		$scope.search = function() {
+			if (isCityNameIncorrect($scope.city)) {
+				alert("Try again, type some more letters!");
+				return;
+			};
+			
+			var url = 'http://api.openweathermap.org/data/2.5/forecast/daily';
+			$http.jsonp(url, { params : {
+				cnt: $scope.days,
+				q : $scope.city,
+				lang: $scope.lang,
+				units : $scope.units,
+				appid: '2de143494c0b295cca9337e1e96b00e0',
+				callback: 'JSON_CALLBACK'
+			  }}).success(function(response) {
+				$scope.showTable = true;
+				$scope.wheatherList = response.list;
+			});				
+		}
     });
-});
+}());
